dc12fdd99161c8c98f583b48e73bb50c3742022b
Hollie.Phillips <moonkatcreations@gmail.com>
2009-07-17 11:42:09 -0500

    Created.

---
+++ imageSwap.js
@@ -0,0 +1,22 @@
+ /**
+     * Image Swap
+     * (like the parties your parents never told you about...)
+     * 
+     * @example <img src="images/someimage.png" swapsrc="images/someotherimage.png">
+     */
+$(document).ready(function(){
+    var swapsrcSelector = "[swapsrc]";
+    var swapsrcTag = "swapsrc";
+    
+    $(swapsrcSelector).each(function() {
+		var me = $(this)
+		var im1 = me.attr("src"), im2 = me.attr(swapsrcTag);
+		me.hover(function() {
+			me.attr("src", im2);
+			me.attr(swapsrcTag, im1);
+		}, function() {
+			me.attr("src", im1);
+			me.attr(swapsrcTag, im2);
+		});
+	});
+});
